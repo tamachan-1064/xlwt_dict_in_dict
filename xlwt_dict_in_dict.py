060ffbf62106423283a47efc943cdcd9a0abcc29
Yuichiro Tamai <tamachan.1064@gmail.com>
2018-06-26 12:40:51 +0900

    final

---
+++ xlwt_dict_in_dict.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 26 10:47:41 2018
+
+@author: tamac
+"""
+
+import xlwt
+import os
+
+alist = ['a1', 'a2','a3']
+klist = ['k1', 'k2','k3']
+d = {'a1':{'k1': 1, 'k2':2, 'k3': 3}, 'a2':{'k1': 4, 'k2':5, 'k3': 6}, 'a3':{'k1': 7, 'k2':8, 'k3': 9}}
+
+wb = xlwt.Workbook()
+
+sheet = wb.add_sheet('sheet1', cell_overwrite_ok=True)
+sheet2 = wb.add_sheet('sheet2', cell_overwrite_ok=True)
+
+for i, key1 in enumerate(alist):
+    for j, key2 in enumerate(klist):
+        sheet.write(i+1, 0, str(key1))
+        sheet.write(0, j+1, str(key2))
+        sheet.write(i+1, j+1, d[key1][key2])
+
+cwd = os.getcwd()
+wb.save(cwd +'/xlwt_sample.xls')
